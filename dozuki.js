133a8e3470925d348159cc6fd1dab1aafe9b94c0
Chris Opperwall <chriso@ifixit.com>
2017-01-23 17:30:32 -0700

    Allow optional query params object to Dozuki.guide.get
    The Guide GET API endpoint allows more query parameters than just a
    langid. This adds an optional `params` object to the get method, which
    are appended to the API URL constructed in the function call.

--- dozuki.js
+++ dozuki.js
@@ -3,10 +3,19 @@
    function Dozuki(url, http) {
       baseUrl = url + "/api/2.0/";
       this.guides = {
-         get: function(guideid, langid) {
+         get: function(guideid, langid, params) {
+            params = params || {};
             var url = baseUrl + "guides/" + guideid;
             if (langid) {
-               url += "?langid=" + langid;
+               params["langid"] = langid;
+            }
+
+            var paramString = Object.keys(params).map(function(key) {
+               return params[key] ? key + "=" + params[key] : key;
+            }).join("&");
+
+            if (paramString) {
+               url += "?" + paramString;
             }
 
             return http.send(
@@ -19,8 +28,8 @@
                   }
                });
          }
-      }
-   }
+      };
+   };
 
    global.Dozuki.http = {};
 })(typeof window != 'undefined' ? window : module.exports);
